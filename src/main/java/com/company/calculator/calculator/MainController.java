71b681f4d7e48ab89c2cfc180a2490cf9e447f24
Zifala Technologies <mohaaosman12@gmail.com>
2023-01-14 11:41:06 +0300

    Added: Operation and input

--- src/main/java/com/company/calculator/calculator/MainController.java
+++ src/main/java/com/company/calculator/calculator/MainController.java
@@ -1,8 +1,145 @@
 package com.company.calculator.calculator;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class MainController {
+    // Global Vars
+    String op;
+    double value1;
+    double value2;
+
+    @FXML
+    public Label result_lbl;
+    @FXML
+    public Label holder_lbl;
+
+    @FXML
+    public void handleOperation(ActionEvent event) {
+        // Get Control value
+        String data = ((MFXButton)event.getSource()).getText();
+        // Choice
+        switch (data) {
+            case "p" -> plusOP();
+            case "s" -> subOP();
+            case "m" -> multiOP();
+            case "d" -> divideOP();
+            case "ca" -> clearOP();
+            case "eq" -> equalOP();
+        }
+    }
+
+    private void equalOP() {
+            switch (op){
+                case "p" -> AddValue();
+                case "s" -> SubValue();
+                case "m" -> MultiplyValue();
+                case "d" -> DivideValue();
+
+            }
+    }
+
+    private void DivideValue() {
+        String rst = result_lbl.getText();
+        value2 = Double.parseDouble(rst);
+        double sum = value1 / value2;
+        value2 = 0;
+        value1 = 0;
+        String pre = holder_lbl.getText();
+        holder_lbl.setText(pre+rst);
+
+        result_lbl.setText(String.valueOf(sum));
+    }
+
+    private void MultiplyValue() {
+        String rst = result_lbl.getText();
+        value2 = Double.parseDouble(rst);
+        double sum = value1 * value2;
+        value2 = 0;
+        value1 = 0;
+        String pre = holder_lbl.getText();
+        holder_lbl.setText(pre+rst);
+
+        result_lbl.setText(String.valueOf(sum));
+    }
+
+    private void SubValue() {
+        String rst = result_lbl.getText();
+        value2 = Double.parseDouble(rst);
+        double sum = value1 - value2;
+        value2 = 0;
+        value1 = 0;
+        String pre = holder_lbl.getText();
+        holder_lbl.setText(pre+rst);
+        result_lbl.setText(String.valueOf(sum));
+    }
+
+    private void AddValue() {
+        // Value 2
+        String rst = result_lbl.getText();
+        value2 = Double.parseDouble(rst);
+        double sum = value1 + value2;
+        value2 = 0;
+        value1 = 0;
+        String pre = holder_lbl.getText();
+        holder_lbl.setText(pre+rst);
+        result_lbl.setText(String.valueOf(sum));
+    }
+
+    private void clearOP() {
+        result_lbl.setText("");
+        holder_lbl.setText("");
+    }
+
+    private void divideOP() {
+        //Set Operation
+        op = "d";
+        // Current value
+        String rst = result_lbl.getText();
+        value1 = Double.parseDouble(rst);
+        holder_lbl.setText(rst+"/");
+        result_lbl.setText("");
+    }
+
+    private void multiOP() {
+        //Set Operation
+        op = "m";
+        // Current value
+        String rst = result_lbl.getText();
+        value1 = Double.parseDouble(rst);
+        holder_lbl.setText(rst+"x");
+        result_lbl.setText("");
+    }
+
+    private void subOP() {
+        //Set Operation
+        op = "s";
+        // Current value
+        String rst = result_lbl.getText();
+        value1 = Double.parseDouble(rst);
+        holder_lbl.setText(rst+"-");
+        result_lbl.setText("");
+    }
+
+    private void plusOP() {
+        //Set Operation
+        op = "p";
+        // Current value
+        String rst = result_lbl.getText();
+        value1 = Double.parseDouble(rst);
+        holder_lbl.setText(rst+"+");
+        result_lbl.setText("");
+    }
+
+    @FXML
+    public void handleInput(ActionEvent event) {
+        // Get Control value
+        String data = ((MFXButton)event.getSource()).getText();
+        String pre = result_lbl.getText();
+        result_lbl.setText(pre+data);
+    }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.company.calculator.calculator {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens com.company.calculator.calculator to javafx.fxml;
+    requires MaterialFX;
     exports com.company.calculator.calculator;
 }
